65744460eff6b35b645c7cd7dd6a18d6d302d069
David Boh <53196387+DavidBoh@users.noreply.github.com>
2022-08-11 08:36:50 -0500

    Update debian11_fresh_install.sh

--- debian11_fresh_install.sh
+++ debian11_fresh_install.sh
@@ -2,8 +2,9 @@
 
 #Script for Debian fresh install. Execute as root
 # Tested functional august 11 2022
-# Modify /etc/apt/sources.list file for debian 11. Includes non free repos. 
 
+
+# Modify /etc/apt/sources.list file for debian 11. Includes non free repos. 
 echo "deb http://deb.debian.org/debian bullseye main contrib non-free" > /etc/apt/sources.list
 echo "deb-src http://deb.debian.org/debian bullseye main contrib non-free" >> /etc/apt/sources.list
 echo "" >> /etc/apt/sources.list
@@ -15,20 +16,23 @@ echo "deb-src http://deb.debian.org/debian bullseye-updates main contrib non-fre
 echo "" >> /etc/apt/sources.list
 echo "# Visit https://wiki.debian.org/SourcesList for more info."
 
-
+# update upgrade
 apt update
 apt dist-upgrade -y
 apt autoremove -y
 
+# remove unwanted apps (feel free to modify)
 apt purge evolution libreoffice* gnome-nibbles gnome-tetravex gnome-taquin tali swell-foop five-or-more gnome-mines gnome-robots quadrapassel hitori gnome-2048 aisleriot iagno gnome-klotski lightsoff four-in-a-row gnome-mahjongg -y
 apt autoremove -y
 
+#remove conflicting package
 apt --purge remove libva2 -y
 
 apt update
 apt dist-upgrade -y
 apt autoremove -y
 
+#install useful apps (fell free to modify)
 apt install pavucontrol python3-pip gcc g++ git virt-manager fish zsh gnome-tweak-tool obs-studio gimp vlc transmission flatpak bridge-utils qemu-kvm tlp tlp-rdw -y
 # configure flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
