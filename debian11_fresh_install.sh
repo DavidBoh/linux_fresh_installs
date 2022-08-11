d28df628f25e542bf6a5d2ade6c8770bd90af621
David Boh <53196387+DavidBoh@users.noreply.github.com>
2022-08-11 07:03:36 -0500

    Create debian11_fresh_install.sh

---
+++ debian11_fresh_install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+#Script for Debian fresh install. Execute as root
+
+# Modify /etc/apt/sources.list file for debian 11. Includes non free repos. 
+
+echo "deb http://deb.debian.org/debian bullseye main contrib non-free" > /etc/apt/sources.list
+echo "deb-src http://deb.debian.org/debian bullseye main contrib non-free" >> /etc/apt/sources.list
+echo "" >> /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian-security/ bullseye-security main contrib non-free" >> /etc/apt/sources.list
+echo "deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib non-free" >> /etc/apt/sources.list
+echo "" >> /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian bullseye-updates main contrib non-free" >> /etc/apt/sources.list
+echo "deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free" >> /etc/apt/sources.list
+echo "" >> /etc/apt/sources.list
+echo "# Visit https://wiki.debian.org/SourcesList for more info."
+
+
+apt update
+apt dist-upgrade -y
+apt autoremove -y
+
+apt purge evolution* libreoffice* gnome-nibbles gnome-tetravex gnome-taquin tali swell-foop five-or-more gnome-mines gnome-robots quadrapassel hitori gnome-2048 aisleriot iagno gnome-klotski lightsoff four-in-a-row gnome-mahjongg-y
+apt autoremove -y
+
+apt update
+apt dist-upgrade -y
+apt autoremove -y
+
+apt install pavucontrol python3-pip gcc g++ git virt-manager fish zsh gnome-shell-extension-dash-to-dock gnome-extensions-app obs-studio gimp vlc transmission flatpak bridge-utils libvirt virt-install qemu-kvm tlp tlp-rdw -y
+# configure flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+#kdenlive thunderbird
+
+systemctl enable tlp
