143e4a73a1395a251997ff95c517469dc12bed8a
David Boh <53196387+DavidBoh@users.noreply.github.com>
2022-08-11 06:29:35 -0500

    Create fedora_fresh_install.sh

---
+++ fedora_fresh_install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#Script for Fedora fresh install. Execute as root
+
+echo "fastestmirror = true" >> /etc/dnf/dnf.conf
+
+# RPM FUSION, MAKE SURE TO KEEP UPDATED
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+dnf groupupdate core -y
+dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+dnf groupupdate sound-and-video -y
+
+dnf upgrade -y
+
+dnf install pavucontrol python3-pip gcc g++ git thunderbird virt-manager fish zsh gnome-shell-extension-dash-to-dock gnome-extensions-app obs-studio gimp vlc transmission mediawriter bridge-utils libvirt virt-install qemu-kvm tlp tlp-rdw -y
+
+#kdenlive   
+
+systemctl enable tlp
