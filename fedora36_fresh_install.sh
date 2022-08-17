047c32aaa650041ad2afb62d6458dfe7d83cc591
David Boh <53196387+DavidBoh@users.noreply.github.com>
2022-08-17 16:29:52 -0500

    Update fedora36_fresh_install.sh

--- fedora36_fresh_install.sh
+++ fedora36_fresh_install.sh
@@ -36,6 +36,8 @@ echo "Installing applications"
 dnf install util-linux-user deja-dup pavucontrol python3-pip python3-devel gcc g++ git thunderbird virt-manager \
 fish zsh gnome-shell-extension-dash-to-dock gnome-extensions-app obs-studio gimp vlc transmission \
 mediawriter bridge-utils libvirt virt-install qemu-kvm tlp tlp-rdw mono-complete nodejs curl wget \
+cmake mono-devel
+
 java-latest-openjdk npm -y
 
 #kdenlive   
@@ -49,6 +51,7 @@ echo "Enabling flatpaks"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 echo "Success"
 
+echo "Installing Go (golang)"
 #Installing Go
 dnf install golang -y
 mkdir -p $HOME/go
@@ -58,12 +61,14 @@ source $HOME/.bashrc
 echo "dnf upgrading"
 dnf upgrade -y
 
+echo "Installing Rust"
 #Installing Rust
 curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh
 
 echo "dnf upgrading"
 dnf upgrade -y
 
+echo "Installing Dotnet"
 #Installing Dotnet 
 dnf install dotnet-sdk-6.0 -y
 dnf install aspnetcore-runtime-6.0 -y
@@ -72,11 +77,69 @@ dnf install dotnet-runtime-6.0 -y
 echo "dnf upgrading"
 dnf upgrade -y
 
+# Installing dev tools
+echo "Installing Dev tools"
+sudo dnf groupinstall "Development Tools" "Development Libraries"
+
 #enable laptop battery optimizer service 
 echo "Activating tlp battery optimizer"
 systemctl enable tlp
 systemctl start tlp
 echo "Success"
+
+echo "setting up .vimrc file"
+#Set up .vimrc
+cat <<EOT >> ~/.vimrc
+set nocompatible              " be iMproved, required
+filetype off                  " required
+
+" set the runtime path to include Vundle and initialize
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+" alternatively, pass a path where Vundle should install plugins
+"call vundle#begin('~/some/path/here')
+
+" let Vundle manage Vundle, required
+Plugin 'VundleVim/Vundle.vim'
+Plugin 'Valloric/YouCompleteMe'
+" The following are examples of different formats supported.
+" Keep Plugin commands between vundle#begin/end.
+" plugin on GitHub repo
+"Plugin 'tpope/vim-fugitive'
+" plugin from http://vim-scripts.org/vim/scripts.html
+" Plugin 'L9'
+" Git plugin not hosted on GitHub
+"Plugin 'git://git.wincent.com/command-t.git'
+" git repos on your local machine (i.e. when working on your own plugin)
+"Plugin 'file:///home/gmarik/path/to/plugin'
+" The sparkup vim script is in a subdirectory of this repo called vim.
+" Pass the path to set the runtimepath properly.
+"Plugin 'rstacruz/sparkup', {'rtp': 'vim/'}
+" Install L9 and avoid a Naming conflict if you've already installed a
+" different version somewhere else.
+" Plugin 'ascenator/L9', {'name': 'newL9'}
+
+" All of your Plugins must be added before the following line
+call vundle#end()            " required
+filetype plugin indent on    " required
+" To ignore plugin indent changes, instead use:
+"filetype plugin on
+"
+" Brief help
+" :PluginList       - lists configured plugins
+" :PluginInstall    - installs plugins; append `!` to update or just :PluginUpdate
+" :PluginSearch foo - searches for foo; append `!` to refresh local cache
+" :PluginClean      - confirms removal of unused plugins; append `!` to auto-approve removal
+"
+" see :h vundle for more details or wiki for FAQ
+" Put your non-Plugin stuff after this line
+set number
+filetype plugin on
+filetype indent on
+set autoindent
+EOT
+
+
 
 # Block distracting websites. Modify as needed. 
 echo "Blocking websites in /etc/hosts file"
